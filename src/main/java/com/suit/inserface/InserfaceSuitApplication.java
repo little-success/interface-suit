1c9b040cb7dfc72ec590d1dbf24b4483370b433c
liutianhua123 <15801631294@163.com>
2020-07-03 10:37:55 +0800

    frist upload

---
+++ src/main/java/com/suit/inserface/InserfaceSuitApplication.java
@@ -0,0 +1,13 @@
+package com.suit.inserface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InserfaceSuitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InserfaceSuitApplication.class, args);
+    }
+
+}
