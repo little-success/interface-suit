4a090b6a15682f921f88e6e74d6e74b002abb677
liutianhua123 <15801631294@163.com>
2020-07-09 16:47:53 +0800

    run testclass

--- src/test/java/com/suit/interfaces/testmapper/MapperCase.java
+++ src/test/java/com/suit/interfaces/testmapper/MapperCase.java
@@ -1,14 +1,27 @@
 package com.suit.interfaces.testmapper;
 
+import com.suit.interfaces.InterfaceSuitApplication;
 import com.suit.interfaces.dao.ParamsMapper;
 import com.suit.interfaces.entity.InterfaceParams;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
+import javax.annotation.Resource;
 import java.util.Iterator;
 import java.util.List;
 
+import static io.restassured.RestAssured.given;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = InterfaceSuitApplication.class)
+@WebAppConfiguration
 public class MapperCase {
 
+    @Resource
     private ParamsMapper paramsMapper ;
     @Test
     public void queryPramsInfo(){
@@ -16,5 +29,14 @@ public class MapperCase {
         for(Iterator it = paramsList.iterator();it.hasNext();){
             System.out.println(it.next());
         }
+    }
+
+    @Test
+    public void testone(){
+        given()
+                .when().log().all()
+                .get("http://127.0.0.1:8089/interface/query?suitId=6")
+                .then().log().all().extract().response()
+                .toString();
     }
 }
